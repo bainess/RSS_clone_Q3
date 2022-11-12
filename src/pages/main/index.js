69929bc372ac078c3fffcfe46956d7703ea3b83a
Inessa Babenko <iness.tt.eln@gmail.com>
2022-11-13 00:43:20 +0700

    feat: add questions block

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,7 +4,7 @@ function openBurgerMenu () {
   let burgerMenu = document.getElementById('burger-menu');
   let burgerLines = Array.from(document.getElementsByClassName('burger-lines'))
   let menuBackground = document.getElementById('burger-background');
-  let menuList = document.getElementsByTagName('nav')[0];
+  let menuList = document.getElementById('navigation');
   burgerMenu.addEventListener('click', (e) => {
     burgerLines.forEach((el, i) => el.classList.toggle('open'));
     menuBackground.classList.toggle('open');
